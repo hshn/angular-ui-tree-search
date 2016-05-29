6f218ad8b37219a8e507be634298895cce289990
Shota Hoshino <sht.hshn@gmail.com>
2016-05-29 22:27:29 +0900

    Added match visitor

---
+++ src/traversal/visitor/match-visitor.js
@@ -0,0 +1,21 @@
+import { Visitor } from './visitor';
+
+export class MatchVisitor extends Visitor {
+  constructor(match) {
+    super();
+
+    this.match = match;
+  }
+
+  enterNode(node) {
+    node = super.enterNode(node);
+
+    node.$matched = this.match(node);
+
+    return node;
+  }
+
+  leaveNode(node) {
+    return super.leaveNode(node);
+  }
+}
---
+++ test/traversal/visitor/match-visitor.spec.js
@@ -0,0 +1,17 @@
+import { MatchVisitor } from '../../../src/traversal/visitor/match-visitor';
+
+describe('MatchVisitorSpec', () => {
+  describe('enterNode()', () => {
+    it('should set $matched true if matched', () => {
+      let visitor = new MatchVisitor(node => true);
+
+      expect(visitor.enterNode({}).$matched).toBe(true);
+    });
+
+    it('should set $matched false if did not match', () => {
+      let visitor = new MatchVisitor(node => false);
+
+      expect(visitor.enterNode({}).$matched).toBe(false);
+    });
+  });
+});
