33e1ec703c6df67b29a0fc19080bc698405104e5
Shota Hoshino <sht.hshn@gmail.com>
2016-05-29 22:16:36 +0900

    Added build settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  metadata: {
+    baseUrl: '/'
+  },
   devtool: 'sourcemap',
   entry: {
     'vendor': './src/vendor.js',
-    'ng-ui-tree-filter': './src/index.js'
+    'demo': './src/demo.js',
+    'index': './src/index.js'
   },
   resolve: {
     modulesDirectories: ['node_modules'],
@@ -16,16 +21,30 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: [/node_modules/], loader: 'ng-annotate!babel' }
+      { test: /\.js$/, exclude: [/node_modules/], loader: 'ng-annotate!babel' },
+      { test: /\/angular\.js$/, loader: 'exports?angular' }
     ]
   },
   plugins: [
     new webpack.ProvidePlugin({
-      'window.angular': 'angular'
+      // 'window.angular': 'angular',
+      // 'angular': 'angular'
     }),
     new webpack.optimize.OccurenceOrderPlugin(true),
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['vendor'].reverse()
+      name: 'vendor',
+      chunks: ['index', 'demo']
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'index',
+      chunks: ['demo']
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/demo.html',
+      chunksSortMode: 'dependency'
     })
-  ]
+  ],
+  devServer: {
+    port: 3000
+  }
 }
